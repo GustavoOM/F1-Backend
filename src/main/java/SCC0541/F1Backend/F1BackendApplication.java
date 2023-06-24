fa6ec0a3670ca5737df1c2ee3744ea3f33c7ee4e
GustavoOM <gumartins2001@gmail.com>
2023-06-24 15:27:14 -0300

    Inicio do projeto

---
+++ src/main/java/SCC0541/F1Backend/F1BackendApplication.java
@@ -0,0 +1,13 @@
+package SCC0541.F1Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SCC0541/F1Backend/F1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SCC0541.F1Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
